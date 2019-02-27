71b14d96106a0e07957a6034b90c664b959ee255
Robert Chen <wchen6572@gmail.com>
2019-02-26 23:20:49 -0600

    Final Commit - Ran out of time. Psuedo coded countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,10 +5,11 @@
 $(document).ready(function() {
     
     var correctAnswers = 0;
+    var countDowntimer = 30;
     var wrongAnswers = 0;
     var skipped = 0;
-    var gameStart = ".endStats"
-    var questions = ".trivia-questions"
+    var gameStart = ".endStats";
+    var questions = ".trivia-questions";
     // var questions = ".response"
 
     $(gameStart).hide();
@@ -16,8 +17,9 @@ $(document).ready(function() {
     
     function displayStats (){
         $("#correctScore").text("Number of Correct Answers " + correctAnswers)
+        $("#wrongScore").text("Number of Correct Answers " + wrongAnswers)
         $(gameStart).show();
-        console.log("inside function")
+        // console.log("inside function")
         
     }
 
@@ -25,13 +27,29 @@ $(document).ready(function() {
     
     $('#game-done').on('click', function(){
         $(questions).hide();
-        console.log("hello");
+        // console.log("hello");
         displayStats();
-        console.log("after display")
+        // console.log("after display")
 
         
         
     });
+    // 
+    // function countdownTimer(){
+    //     intervalId = setInterval(decrement, 1000);
+    // }
+
+  
+    // function decrement(){
+    //     countDowntimer--;
+    //     $('#countdown').html(" " + countDowntimer + " " + "seconds");
+    //     if (number ===1){
+            // print time on screen
+    //     }
+    //     else if(number ===0) {
+            // if you run out of time call on the displayStats function
+    //     }
+    // }
     
     
     
